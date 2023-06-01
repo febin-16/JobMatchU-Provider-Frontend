e6d05232457356683582150bfc1144bf745c710c
febin-16 <febinparappuram@gmail.com>
2023-06-01 18:47:38 +0530

    category included

--- src/api/GetProviderId.jsx
+++ src/api/GetProviderId.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { BASE_URL } from "../constants/urls";
 export async function getProviderId(user){
         console.log(user);
-        const url=BASE_URL+`api/company/profileUpdate`;
+        const url=BASE_URL+`api/company/profileupdate`;
         try
         {
             const response = await axios.get(url,{params:{"username":user}});
